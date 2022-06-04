fa568cd79469ce1b2da488daf9f1c55d32e3a4ad
wambuingare <wambuinancyngare@gmail.com>
2022-06-04 19:51:37 +0300

    sign_up

--- app/src/main/java/gym/mima/workoutlog/LoginActivity.kt
+++ app/src/main/java/gym/mima/workoutlog/LoginActivity.kt
@@ -1,13 +1,16 @@
 package gym.mima.workoutlog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
     lateinit var btnLogin:Button
+    lateinit var tvSignup:TextView
     lateinit var tilEmail:TextInputLayout
     lateinit var tilPassword:TextInputLayout
     lateinit var etEmail:TextInputEditText
@@ -17,14 +20,18 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         btnLogin=findViewById(R.id.btnLogin)
+        tvSignup=findViewById(R.id.tvSignup)
         tilEmail=findViewById(R.id.tilEmail)
         tilPassword=findViewById(R.id.tilPassword)
         etEmail=findViewById(R.id.etEmail)
         etPassword=findViewById(R.id.etPassword)
-
         btnLogin.setOnClickListener {
             validateLogin()
 
+        }
+        tvSignup.setOnClickListener {
+            val intent=Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
         }
     }
 
---
+++ app/src/main/java/gym/mima/workoutlog/SignUpActivity.kt
@@ -0,0 +1,75 @@
+package gym.mima.workoutlog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class SignUpActivity : AppCompatActivity() {
+        lateinit var btnSign_Up: Button
+        lateinit var tvLog_in: TextView
+        lateinit var tilFirstname: TextInputLayout
+        lateinit var etfirstname: TextInputEditText
+        lateinit var tilSecondName: TextInputLayout
+        lateinit var etlastname: TextInputEditText
+        lateinit var tilemail: TextInputLayout
+        lateinit var etemail: TextInputEditText
+        lateinit var tilPassword: TextInputLayout
+        lateinit var etPassword: TextInputEditText
+        lateinit var tilconfirm: TextInputLayout
+        lateinit var etconfirm: TextInputEditText
+
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_sign_up)
+
+            btnSign_Up=findViewById(R.id.btnSign_Up)
+            tvLog_in=findViewById(R.id.tvLog_in)
+            tilFirstname=findViewById(R.id.tilFirstname)
+            etfirstname=findViewById(R.id.etfirstname)
+            tilSecondName=findViewById(R.id.tilSecondName)
+            etlastname=findViewById(R.id.etlastname)
+            tilemail=findViewById(R.id.tilemail)
+            etemail=findViewById(R.id.etemail)
+            tilPassword=findViewById(R.id.tilPassword)
+            etPassword=findViewById(R.id.etPassword)
+            tilconfirm=findViewById(R.id.tilconfirm)
+            etconfirm=findViewById(R.id.etconfirm)
+
+            btnSign_Up.setOnClickListener {
+                validateSignUp()
+            }
+            tvLog_in.setOnClickListener {
+                val intent=Intent(this,LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        fun validateSignUp(){
+           var firstname=etfirstname.text.toString()
+           var lastname=etlastname.text.toString()
+           var email=etemail.text.toString()
+           var password=etPassword.text.toString()
+           var confirm=etconfirm.text.toString()
+
+           if (firstname.isBlank()) {
+               tilFirstname.error = getString(R.string.enter_firstname)
+           }
+           if (lastname.isBlank()) {
+               tilSecondName.error = getString(R.string.enter_lastname)
+           }
+           if (email.isBlank()) {
+               tilemail.error = getString(R.string.email_required)
+           }
+           if (password.isBlank()) {
+              tilPassword.error = getString(R.string.password_required)
+           }
+           if (confirm.isBlank()) {
+              tilconfirm.error = getString(R.string.confirm_password)
+           }
+       }
+
+    }
