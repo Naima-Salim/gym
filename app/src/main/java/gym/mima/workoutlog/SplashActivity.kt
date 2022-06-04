d5ac1690046cbd2d971814fcc8c084634fee82ea
wambuingare <wambuinancyngare@gmail.com>
2022-06-04 08:21:18 +0300

    splash-activity

---
+++ app/src/main/java/gym/mima/workoutlog/SplashActivity.kt
@@ -0,0 +1,14 @@
+package gym.mima.workoutlog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(Intent(this,LoginActivity::class.java))
+        finish()
+    }
+}
